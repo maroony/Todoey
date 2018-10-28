748587b46da4ef0f1fa399e2770b22e3c6d63802
Andy Schoenemann <AndySchoenemann@gmail.com>
2018-10-28 14:34:14 +0100

    Adding items works just fine

---
+++ Todoey/TodoListViewController.swift
@@ -0,0 +1,65 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Andy on 28.10.18.
+//  Copyright © 2018 Andy Schoenemann. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+  var itemArray = ["Find Mike", "Buy Eggs", "Destroy Something"]
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  // MARK: - Tableview Datasource Methods
+
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
+    cell.textLabel?.text = itemArray[indexPath.row]
+    return cell
+  }
+
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return itemArray.count
+  }
+
+  override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    print(indexPath.row)
+    tableView.deselectRow(at: indexPath, animated: true)
+
+    if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+      tableView.cellForRow(at: indexPath)?.accessoryType = .none
+    } else {
+      tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+    }
+  }
+
+  // Mark: - Add new item
+
+  @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+    let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert)
+    var textField = UITextField()
+
+    alert.addTextField { alertTextField in
+      alertTextField.placeholder = "Create new item"
+      textField = alertTextField
+    }
+
+    let action = UIAlertAction(title: "Add Item", style: .default) { _ in
+      if let text = textField.text {
+        if text.trimmingCharacters(in: [" "]) != "" {
+          self.itemArray.append(text)
+          self.tableView.reloadData()
+        }
+      }
+    }
+
+    alert.addAction(action)
+    present(alert, animated: true, completion: nil)
+  }
+}
